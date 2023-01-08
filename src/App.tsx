03fef913869e727cac91b1abe7368f4983b2f5d1
seoyeon <wjstjdus96@naver.com>
2023-01-09 02:03:55 +0900

    feat: Coin페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,8 @@ table {
 body{
   font-family: 'Source Sans Pro', sans-serif;
   background-color: ${(props) => props.theme.bgColor};
-  color:${(props) => props.theme.textColor}
+  color:${(props) => props.theme.textColor};
+  line-height: 1.2;
 }
 a{
   text-decoration: none;
