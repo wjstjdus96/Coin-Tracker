4ccd026fb6e02bcb6252402f49079883140a45bc
seoyeon <wjstjdus96@naver.com>
2023-01-09 02:40:33 +0900

    fix: react-query로 데이터 fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Router from "./Router";
 import { createGlobalStyle } from "styled-components";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 const GlobalStyle = createGlobalStyle`@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
 html, body, div, span, applet, object, iframe,
@@ -65,6 +66,7 @@ function App() {
     <>
       <GlobalStyle />
       <Router />
+      <ReactQueryDevtools initialIsOpen={true} />
     </>
   );
 }
