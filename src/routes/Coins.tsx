76d52d37d2e0bb6ed8c5f287f6d662a3b6b986bb
seoyeon <wjstjdus96@naver.com>
2023-01-11 19:07:56 +0900

    feat: 다크모드 기능 구현

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -26,10 +26,11 @@ const Loader = styled.span`
 const CoinsList = styled.ul``;
 
 const Coin = styled.li`
-  background-color: white;
-  color: ${(props) => props.theme.bgColor};
+  background-color: ${(props) => props.theme.cardBgColor};
+  color: ${(props) => props.theme.textColor};
   border-radius: 15px;
   margin-bottom: 10px;
+  border: 1px solid white;
   a {
     padding: 20px;
     transition: color 0.2s ease-in;
